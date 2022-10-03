0dc6db6117136c9fa2e6a001c4de3376ccbd45b6
Jeff Phillips <jefphi@pennmedicine.upenn.edu>
2022-10-03 11:52:26 -0400

    Specify script directory

--- run_pet_quants.sh
+++ run_pet_quants.sh
@@ -3,36 +3,9 @@
 # Follows BIDS PET standard and expects BIDS-format filenames. 
 # Note that subject and session labels cannot contain BIDS-incompatible 
 # characters like underscores or periods.
-
-#
-# Initial comment to check pull requests
-# 
-
-# Load required software on PMACS LPC.
-module load ANTs/2.3.5
-module load afni_openmp/20.1
-module load PETPVC/1.2.10
-module load fsl/6.0.3
-
-# JSP: If we can find an alternative to copying the template and associated labels and warps from the ANTsCT container,
-# we can get rid of the singularity call.
-module load DEV/singularity
-
 # Command-line arguments.
 petName=$1 # Absolute path of BIDS-format, attentuation-corrected dynamic PET image
 t1Name=$2 # Absolute path of N4-corrected, skull-on T1 image from ANTsCT output directory
-
-# JSP: required files include:
-# petName=$1 # Absolute path of BIDS-format, attentuation-corrected dynamic PET image
-# t1Name=$2 # Absolute path of N4-corrected, skull-on T1 image from ANTsCT output directory
-# bmaskName=${t1dir}/sub-${id}_ses-${mrisess}_BrainExtractionMask.nii.gz
-# segName=${t1dir}/sub-${id}_ses-${mrisess}_BrainSegmentation.nii.gz
-# T1-template transforms:
-#   ${t1dir}/*_SubjectToTemplate1Warp.nii.gz
-#   ${t1dir}/*_SubjectToTemplate0GenericAffine.mat`
-#   ${t1dir}/*_TemplateToSubject0Warp.nii.gz
-#   ${t1dir}/*_TemplateToSubject1GenericAffine.mat`
-# Tissue probability posterior images: ${t1dir}/*Posteriors[1-6]nii.gz 
 
 # Record job ID.
 # JSP: useful for job monitoring and debugging failed jobs.
@@ -55,15 +28,16 @@ wd=${petdir/sub-${id}\/ses-${petsess}} # Subjects directory
 pfx="${outdir}/sub-${id}_ses-${petsess}_trc-${trc}"
 
 # Python environment.
-source /project/ftdc_misc/software/pkg/miniconda3/bin/activate; conda activate flywheel
+source /project/ftdc_misc/software/pkg/miniconda3/bin/activate
+conda activate flywheel
 
 # Get label statistics for multiple atlases using QuANTs.
 for metricFile in "${pfx}_desc-suvr${mrisess}_pet.nii.gz" "${pfx}_desc-IY${mrisess}_pet.nii.gz" "${pfx}_desc-RVC${mrisess}_pet.nii.gz"; do
-    python pet_quants.py ${metricFile} ${t1dir}
+    python ${scriptdir}/pet_quants.py ${metricFile} ${t1dir}
 done
 
 # JSP: need to at least make the template directory writeable; otherwise, if the script crashes out, it can't be deleted.
 chgrp -R ftdclpc ${outdir}
 chmod -R 775 ${outdir}
 
-rm -r ${outdir}/template
+rm -rf ${outdir}/template
