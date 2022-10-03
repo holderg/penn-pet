3b7d2afa21fd59d5feee8fb9bce59a77c650653d
Jeff Phillips <jefphi@pennmedicine.upenn.edu>
2022-10-02 23:41:21 -0400

    Run QuANTs only after PET data are processed

---
+++ bsub_pet_quants.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Just run QuANTs on already pre-processed PET dadta.
+# infile: a two-column csv file, no header row, with the full paths to the
+# unprocessed PET file and the T1 BrainExtraction0N4 image from the ANTsCT
+# output directory.
+
+infile=${1}
+scriptdir=`dirname $0`
+
+cat ${infile} | while IFS="," read f t1; do
+	fbase=`basename ${f}`
+	fp=(${fbase//_/\ })
+	subj=${fp[0]}
+	petsess=${fp[1]}
+	wd=/project/ftdc_pipeline/data/pet/${subj}/${petsess}
+	if [[ ! -d ${wd} ]]; then mkdir -p ${wd}; fi
+	cmd="bsub -J pet_quants_${subj}_${petsess} -o ${wd}/%J.stdout -e ${wd}/%J.stderr ${scriptdir}/run_pet_quants.sh ${f} ${t1}"
+	echo $cmd
+	$cmd
+done
+
