1111d28033b402c06eeae2ace12fd51fa9f67e2f
Jeff Phillips <jefphi@ftdcsub1.pmacs.upenn.edu>
2023-06-01 15:17:23 -0400

    Replace MI with Mattes

--- pet_proc_alt_reg.sh
+++ pet_proc_alt_reg.sh
@@ -116,7 +116,7 @@ fi
 # Run affine registration between PET and T1 images.
 echo "Running antsRegistration between PET and T1 images..."
 petxfm="${pfx}_desc-rigid${mrisess}_0GenericAffine.mat"
-mpar="MI[ ${t1Name}, ${pfx}_desc-mean_pet.nii.gz, 1, 64, regular, 0.8]"
+mpar="Mattes[ ${t1Name}, ${pfx}_desc-mean_pet.nii.gz, 1, 128, regular, 0.8]"
 cpar="[500x1000x1000x1000,1.e-6,20]"
 spar="3x2x1x0"
 fpar="4x4x2x1"
